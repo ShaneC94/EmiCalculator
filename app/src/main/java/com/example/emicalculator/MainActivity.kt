a4866c43256002610496365922998eab14c51d3e
ShaneC94 <shane.currie@ontariotechu.net>
2025-09-27 09:49:40 -0400

    feat: created EMI and Budget activities with updates to Main activity for navigation

--- app/src/main/java/com/example/emicalculator/MainActivity.kt
+++ app/src/main/java/com/example/emicalculator/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.emicalculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//Main screen with 2 buttons that go to the EMI Calculator or to the Budget
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val goToEmi = findViewById<Button>(R.id.goToEmi)
+        val goToBudget = findViewById<Button>(R.id.goToBudget)
+
+
+        //navigation using intents
+        goToEmi.setOnClickListener {
+            val intent = Intent(this, EmiActivity::class.java)
+            startActivity(intent)
         }
+
+        goToBudget.setOnClickListener {
+            val intent = Intent(this, BudgetActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
