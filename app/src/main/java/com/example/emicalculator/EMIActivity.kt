a50a9337bbbee82a70c6f1cc40274d999ee756a9
ShaneC94 <shane.currie@ontariotechu.net>
2025-09-27 16:06:57 -0400

    feat: update EMI and xml. Add "white" to colors.xml

--- app/src/main/java/com/example/emicalculator/EMIActivity.kt
+++ app/src/main/java/com/example/emicalculator/EMIActivity.kt
@@ -1,4 +1,33 @@
 package com.example.emicalculator
 
-class EMIActivity {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.badge.BadgeState
+import kotlin.math.pow
+
+//Calculate the monthly EMI
+class EMIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emi)
+
+
+        val loanInput = findViewById<EditText>(R.id.inputLoan)
+        val rateInput = findViewById<EditText>(R.id.inputRate)
+        val tenureInput = findViewById<EditText>(R.id.inputTenure)
+        val calcBtn = findViewById<Button>(R.id.calculateBtn)
+        val resultView = findViewById<TextView>(R.id.displayEmi)
+
+        calcBtn.setOnClickListener{
+            val principal
+            val annualRate
+            val months
+            val monthlyRate = annualRate / 12 / 100
+            val emi (if else logic?)
+            resultView.text = "Monthly EMI = %.2f".format(emi)
+        }
+    }
 }
